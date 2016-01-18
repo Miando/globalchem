b95729d8d3197aafe22a9223248f4996d03b7937
miando <kuznecov513@gmail.com>
2016-01-18 20:28:15 +0200

    3-j

--- mysite/katalog/views.py
+++ mysite/katalog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def tovar_list(request):
+    return render(request, 'katalog/tovar_list.html', {})
