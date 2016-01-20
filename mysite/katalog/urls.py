f48d6eb74b8db5d6da10e56837becbbc676e3f1b
miando <kuznecov513@gmail.com>
2016-01-20 13:31:00 +0200

    5-j

--- mysite/katalog/urls.py
+++ mysite/katalog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.tovar_list, name='tovar_list'),
+    url(r'^zasib/(?P<pk>[0-9]+)/$', views.tovar_detail, name='tovar_detail'),
 ]
--- mysite/katalog/views.py
+++ mysite/katalog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Tovar
+from django.shortcuts import render, get_object_or_404
 
 def tovar_list(request):
     tovars=Tovar.objects.filter(category__name='Moloko')
     return render(request, 'katalog/tovar_list.html', {'tovars':tovars})
+
+def tovar_detail(request, pk):
+    tovar = get_object_or_404(Tovar, pk=pk)
+    return render(request, 'katalog/tovar_detail.html', {'tovar': tovar})
