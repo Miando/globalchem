209c921e533e25af38f58ffd554d7dc15ec9e459
miando <kuznecov513@gmail.com>
2016-01-21 17:36:19 +0200

    7-j

--- mysite/katalog/views.py
+++ mysite/katalog/views.py
@@ -3,6 +3,21 @@ from django.http import HttpResponse
 from .models import Tovar
 from django.shortcuts import render, get_object_or_404
 
+def home(request):
+    return render(request, 'katalog/home.html', {'home':home})
+
+def produkcia(request):
+    return render(request, 'katalog/produkcia.html', {'produkcia':produkcia})
+
+def kontakti(request):
+    return render(request, 'katalog/kontakti.html', {'kontakti':kontakti})
+
+def pro_nas(request):
+    return render(request, 'katalog/pro_nas.html', {'pro_nas':pro_nas})
+
+def sertifikati(request):
+    return render(request, 'katalog/sertifikati.html', {'sertifikati':sertifikati})
+
 def moloko_list(request):
     moloko=Tovar.objects.filter(category__name='moloko')
     return render(request, 'katalog/moloko_list.html', {'moloko':moloko})
@@ -21,4 +36,4 @@ def gigiena_list(request):
 
 def tovar_detail(request, pk):
     tovar = get_object_or_404(Tovar, pk=pk)
-    return render(request, 'katalog/tovar_detail.html', {'tovar': tovar})
+    return render(request, 'katalog/tovar_detail.html', {'tovar':tovar})
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from katalog import views
 
 urlpatterns = [
     url(r'^produkcia/', include('katalog.urls')),
+    url(r'^produkcia/$',  views.produkcia, name='produkcia'),
     url(r'^admin/', include(admin.site.urls)),
-    
+    url(r'^sertifikati/$', views.sertifikati, name='sertifikati'),
+    url(r'^pro_nas/$', views.pro_nas, name='pro_nas'),
+    url(r'^kontakti/$', views.kontakti, name='kontakti'),
+    url(r'', include('katalog.urls')),
 ]
